15bfb4427592b2bdc0d31c85eb9d2ef6fc4f8d10
Lars <laha0006@stud.kea.dk>
2024-02-06 14:36:59 +0100

    Initial commit

---
+++ src/main/java/dev/tolana/touristguide_endpoints/TouristGuideEndpointsApplication.java
@@ -0,0 +1,13 @@
+package dev.tolana.touristguide_endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristGuideEndpointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristGuideEndpointsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/tolana/touristguide_endpoints/TouristGuideEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tolana.touristguide_endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristGuideEndpointsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
